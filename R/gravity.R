d2bcf840c6330475e69da10d388208a6893ff998
Lino Galiana <lino.galiana@insee.fr>
2019-09-27 15:47:59 +0200

    Build package

---
+++ R/gravity.R
@@ -0,0 +1,10 @@
+#' gravity
+#' 
+#' Gravity model with large scale data
+#' 
+#' @docType package
+#' @author Lino Galiana
+#' @importFrom Rcpp evalCpp
+#' @useDynLib gravity
+#' @name gravity
+NULL
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,49 +6,6 @@
 
 using namespace Rcpp;
 
-// rcppeigen_hello_world
-Eigen::MatrixXd rcppeigen_hello_world();
-RcppExport SEXP _gravity_rcppeigen_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcppeigen_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcppeigen_outerproduct
-Eigen::MatrixXd rcppeigen_outerproduct(const Eigen::VectorXd& x);
-RcppExport SEXP _gravity_rcppeigen_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcppeigen_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcppeigen_innerproduct
-double rcppeigen_innerproduct(const Eigen::VectorXd& x);
-RcppExport SEXP _gravity_rcppeigen_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcppeigen_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcppeigen_bothproducts
-Rcpp::List rcppeigen_bothproducts(const Eigen::VectorXd& x);
-RcppExport SEXP _gravity_rcppeigen_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const Eigen::VectorXd& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcppeigen_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // score
 double score(int n, double th, NumericVector mu, NumericVector y);
 RcppExport SEXP _gravity_score(SEXP nSEXP, SEXP thSEXP, SEXP muSEXP, SEXP ySEXP) {
@@ -94,10 +51,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_gravity_rcppeigen_hello_world", (DL_FUNC) &_gravity_rcppeigen_hello_world, 0},
-    {"_gravity_rcppeigen_outerproduct", (DL_FUNC) &_gravity_rcppeigen_outerproduct, 1},
-    {"_gravity_rcppeigen_innerproduct", (DL_FUNC) &_gravity_rcppeigen_innerproduct, 1},
-    {"_gravity_rcppeigen_bothproducts", (DL_FUNC) &_gravity_rcppeigen_bothproducts, 1},
     {"_gravity_score", (DL_FUNC) &_gravity_score, 4},
     {"_gravity_info", (DL_FUNC) &_gravity_info, 4},
     {"_gravity_speed_theta_ml", (DL_FUNC) &_gravity_speed_theta_ml, 5},
