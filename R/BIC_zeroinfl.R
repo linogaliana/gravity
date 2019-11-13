6946dea6e8c677ab1e84b87bace7bbd4bc14f7c6
Lino Galiana <lino.galiana@insee.fr>
2019-11-13 14:54:55 +0100

    Document BIC method

--- R/BIC_zeroinfl.R
+++ R/BIC_zeroinfl.R
@@ -1,14 +1,19 @@
 #' Bayesian information criterion implementation for \code{zeroinfl} objects
 #' 
-#' @inheritParams stats::AIC
+#' @param object A fitted model object for which there exists a logLik
+#'  method to extract the corresponding log-likelihood, or
+#'  an object inheriting from class logLik.
+#' @param ... Optionally more fitted model objects
 #' 
-#' Formula \eqn{-2*l + k*\log(npar)} where \eqn{npar} 
+#' 
+#' Formula \eqn{-2l + k\log(npar)} where *npar*
 #'  represents the number of parameters in the fitted model
 #'  and \eqn{l} represents model log-likelihood
 #' 
+#' @import stats
 #' @export
 
-BIC.zeroinfl <- function(object){
+BIC.zeroinfl <- function(object, ...){
   # We fit 2 objects coefficients + theta parameter
   k <- length(object$coefficients$count) + length(object$coefficients$zero)
   if (!is.null(object$theta)) k <- k + 1
