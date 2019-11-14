d6ffaef0cb4f33de2ad710d97820e4bea023c26b
Lino Galiana <lino.galiana@insee.fr>
2019-11-14 15:12:47 +0100

    Create a template for latex tables

---
+++ R/signif_stars.R
@@ -0,0 +1,7 @@
+signif_stars <- function(pvalue){
+  if (is.na(pvalue)) return("")
+  if (pvalue<0.01) return("$^{***}$")
+  if (pvalue<0.05) return("$^{**}$") 
+  if (pvalue<0.1) return("$^{*}$") 
+  return("")
+}
