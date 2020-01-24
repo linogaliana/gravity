34e8149cf7ae9a9a27bb359ac04bead2a998b883
Lino Galiana <lino.galiana@insee.fr>
2020-01-24 15:49:47 +0100

    Updates

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,52 @@
 
 using namespace Rcpp;
 
+// logistic_reg
+Rcpp::NumericVector logistic_reg(Rcpp::NumericMatrix x, Rcpp::NumericVector y);
+RcppExport SEXP _gravity_logistic_reg(SEXP xSEXP, SEXP ySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type x(xSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type y(ySEXP);
+    rcpp_result_gen = Rcpp::wrap(logistic_reg(x, y));
+    return rcpp_result_gen;
+END_RCPP
+}
+// logit
+Rcpp::NumericVector logit(Rcpp::NumericVector x);
+RcppExport SEXP _gravity_logit(SEXP xSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(logit(x));
+    return rcpp_result_gen;
+END_RCPP
+}
+// invlogit
+Rcpp::NumericVector invlogit(Rcpp::NumericVector x);
+RcppExport SEXP _gravity_invlogit(SEXP xSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(invlogit(x));
+    return rcpp_result_gen;
+END_RCPP
+}
+// invprobit
+Rcpp::NumericVector invprobit(Rcpp::NumericVector x, double eps);
+RcppExport SEXP _gravity_invprobit(SEXP xSEXP, SEXP epsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< double >::type eps(epsSEXP);
+    rcpp_result_gen = Rcpp::wrap(invprobit(x, eps));
+    return rcpp_result_gen;
+END_RCPP
+}
 // score
 double score(int n, double th, NumericVector mu, NumericVector y);
 RcppExport SEXP _gravity_score(SEXP nSEXP, SEXP thSEXP, SEXP muSEXP, SEXP ySEXP) {
@@ -51,6 +97,10 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_gravity_logistic_reg", (DL_FUNC) &_gravity_logistic_reg, 2},
+    {"_gravity_logit", (DL_FUNC) &_gravity_logit, 1},
+    {"_gravity_invlogit", (DL_FUNC) &_gravity_invlogit, 1},
+    {"_gravity_invprobit", (DL_FUNC) &_gravity_invprobit, 2},
     {"_gravity_score", (DL_FUNC) &_gravity_score, 4},
     {"_gravity_info", (DL_FUNC) &_gravity_info, 4},
     {"_gravity_speed_theta_ml", (DL_FUNC) &_gravity_speed_theta_ml, 5},
