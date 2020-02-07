436a56b8b72ae5b97163d45636f5c05bff1dc7e0
Lino Galiana <lino.galiana@insee.fr>
2020-02-07 15:12:36 +0100

    Clean gradient computation

--- R/grad_log_likelihood.R
+++ R/grad_log_likelihood.R
@@ -8,7 +8,7 @@ grad_ZIP_R <- function(params, X, Z, Y,
   
   if (missing(weights)) weights <- rep(1,nrow(X))
   if (missing(offsetx)) offsetx <- rep(0,nrow(X))
-  if (missing(offsetz)) offsetz <- rep(1,nrow(Z))
+  if (missing(offsetz)) offsetz <- rep(0,nrow(Z))
   
   
   kx <- ncol(X)
@@ -23,20 +23,21 @@ grad_ZIP_R <- function(params, X, Z, Y,
                       offsetz)
   muz <- linkinv(etaz)
   
+  
   clogdens0 <- -mu
   
-  Y0 <- (Y>0)
+  Y1 <- (Y>0)
   
-  dens0 <- muz * (1 - as.numeric(!Y0)) + exp(log(1 - muz) + 
+  dens0 <- muz * (1 - as.numeric(Y1)) + exp(log(1 - muz) + 
                                               clogdens0)
-  
-  wres_count <- ifelse(!Y0, Y - mu, -exp(-log(dens0) + 
+  wres_count <- ifelse(Y1, Y - mu, -exp(-log(dens0) + 
                                           log(1 - muz) + clogdens0 + log(mu)))
   
-  wres_zero <- ifelse(!Y0, -1/(1 - muz) * linkobj$mu.eta(etaz), 
-                      (linkobj$mu.eta(etaz) - exp(clogdens0) * linkobj$mu.eta(etaz))/dens0)
   
+  wres_zero <- ifelse(Y1, -1/(1 - muz) * linkobj$mu.eta(etaz), 
+                      (linkobj$mu.eta(etaz) - exp(clogdens0) * linkobj$mu.eta(etaz))/dens0)
   colSums(cbind(wres_count * weights * X, wres_zero * 
                   weights * Z))
-  
+  return(wres_zero)
 }
+
