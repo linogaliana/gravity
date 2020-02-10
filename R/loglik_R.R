b3218bfa90f7c9218d7dd451f7f693a2d00f6cce
Lino Galiana <lino.galiana@insee.fr>
2020-02-10 09:09:16 +0100

    Default offset should be 0

--- R/loglik_R.R
+++ R/loglik_R.R
@@ -8,7 +8,7 @@ loglik_ZIP_R <- function(params, X, Z, Y,
   
   if (missing(weights)) weights <- rep(1,nrow(X))
   if (missing(offsetx)) offsetx <- rep(0,nrow(X))
-  if (missing(offsetz)) offsetz <- rep(1,nrow(Z))
+  if (missing(offsetz)) offsetz <- rep(0,nrow(Z))
   
   
   kx <- ncol(X)
@@ -39,7 +39,7 @@ loglik_ZINB_R <- function(params, X, Z, Y,
   
   if (missing(weights)) weights <- rep(1,nrow(X))
   if (missing(offsetx)) offsetx <- rep(0,nrow(X))
-  if (missing(offsetz)) offsetz <- rep(1,nrow(Z))
+  if (missing(offsetz)) offsetz <- rep(0,nrow(Z))
   
   link <- match.arg(link)
   
